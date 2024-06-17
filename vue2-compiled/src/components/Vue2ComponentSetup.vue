d0cf1eb69d7bdd038dc945983e0142490038ba62
james.sullivan <james.sullivan@pathai.com>
2024-06-17 16:49:18 -0400

    lock file updates

--- vue2-compiled/src/components/Vue2ComponentSetup.vue
+++ vue2-compiled/src/components/Vue2ComponentSetup.vue
@@ -1,12 +1,12 @@
 <script setup>
 import { ref } from 'vue';
-
+console.log('This setup is ran');
 const hello_vue_team = ref(['hello', 'vue', 'team'])
 </script>
 
 <template>
   <div>
-    <h1>Vue3 Component</h1>
+    <h1>Vue2 Component (Crashes)</h1>
     <template v-for="item in hello_vue_team">
       <div :key="item">{{ item }}</div>
     </template>
